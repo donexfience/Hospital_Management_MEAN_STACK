276515412c39efa9c97a2bd2d080f85e0be3f7ee
donexfience <donex6fience@gmail.com>
2024-10-14 21:10:02 +0530

    karma config add (#1) package.json karma CLI added

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+// Polyfills for Angular
+import 'zone.js';  // Included with Angular CLI.
+
+import 'core-js/features/reflect';
+import 'core-js/features/symbol';
+import 'core-js/features/array/includes';
+import 'core-js/features/object/assign';
+
+// Add any additional polyfills below
